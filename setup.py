aed26da35533e7bb1edba2f475bb451ad84e52c7
Mellanfrost <mellanfrost@gmail.com>
2024-09-20 09:44:33 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="twitch_bot",
+    version="0.1.0",
+    py_modules=["twitch_bot"],
+    install_requires=requirements,
+    url="https://github.com/Mellanfrost/twitch-bot",
+    author="Mellanfrost",
+)
